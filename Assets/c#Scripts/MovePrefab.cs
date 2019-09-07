a534f6410c2f6e5f8926a411fd906dec935b580e
unknown <usharani.kotian390@gmail.com>
2019-09-07 23:27:56 +1200

    Enable Fuelcan for endless background

--- Assets/c#Scripts/MovePrefab.cs
+++ Assets/c#Scripts/MovePrefab.cs
@@ -6,17 +6,18 @@ public class MovePrefab : MonoBehaviour
 {
     public GameObject prefabToMove;
     public GameObject currentPrefab;
-    public bool isFirstTrigger;
+    public bool isFirstPrefab;
+    public GameObject fuelCanAnim;
     
     private float incrementX = 50.47f;
 
-    public bool IsFirstTrigger { get => isFirstTrigger; set => isFirstTrigger = value; }
+    
 
-    //  private bool isTriggerAdded = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        //IsFirstTrigger = true;
+        
     }
 
     // Update is called once per frame
@@ -27,15 +28,16 @@ public class MovePrefab : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (IsFirstTrigger)
+        if (isFirstPrefab)
         {
-            IsFirstTrigger = false;
+            isFirstPrefab = false;
         }
         else
         {
             Vector2 temp = new Vector2(currentPrefab.transform.position.x + incrementX, currentPrefab.transform.position.y);
             //  Vector2 temp = new Vector2(81.71f,0.27f);
             prefabToMove.transform.position = temp;
+            fuelCanAnim.SetActive(true);
         }
        
         
